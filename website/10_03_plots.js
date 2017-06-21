ee23e66a9e1699b02673b261fb3a9aa02719df52
DariaDicu <daria.dicu@gmail.com>
2017-06-21 22:51:18 +0100

    Update 10_03_plots.js

--- website/10_03_plots.js
+++ website/10_03_plots.js
@@ -1,4 +1,4 @@
-$.getJSON('http://localhost:8000/x0_p_joint_10_3_17.json/', function (data) {
+$.getJSON('http://localhost:8000/x0_p_joint_10_3_17.json/', {}, function (data) {
 	 var myChart = Highcharts.chart('10_03_x0_p_sing_results', {
 	    chart: {
 	        type: 'scatter'
